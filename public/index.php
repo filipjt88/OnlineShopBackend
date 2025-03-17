e193c05e735092eeb8834d1fea058aab276587da
filipjt88 <filip88ks@gmail.com>
2025-03-17 22:39:04 +0100

    save

--- public/index.php
+++ public/index.php
@@ -1,23 +1,34 @@
 <?php
-
-session_start();
-
 require_once '../app/controllers/ProductController.php';
 require_once '../app/controllers/UserController.php';
+require_once '../app/controllers/CartController.php';
 
-$uri = $_SERVER['REQUEST_URI'];
-$uriParts = explode('/', trim($uri, '/'));
+$requestUri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
 
-if ($uriParts[0] === 'login' && isset($uriParts[1]) && $uriParts[1] === 'authenticate') {
-    $controller = new UserController();
-    $controller->authenticate();
-} elseif ($uriParts[0] === 'register') {
-    $controller = new UserController();
-    $controller->store();
-} elseif ($uriParts[0] === 'products') {
+if ($requestUri[0] === 'products') {
     $controller = new ProductController();
-    $controller->index();
-} elseif (isset($uriParts[0]) && is_numeric($uriParts[0])) {
-    $controller = new ProductController();
-    $controller->show($uriParts[0]);
+    if (isset($requestUri[1]) && is_numeric($requestUri[1])) {
+        $controller->show($requestUri[1]);
+    } else {
+        $controller->index();
+    }
+} elseif ($requestUri[0] === 'users') {
+    $controller = new UserController();
+    if ($requestUri[1] === 'register') {
+        $controller->register();
+    } elseif ($requestUri[1] === 'login') {
+        $controller->login();
+    }
+} elseif ($requestUri[0] === 'cart') {
+    $controller = new CartController();
+    if ($requestUri[1] === 'add') {
+        $controller->add();
+    } elseif ($requestUri[1] === 'get') {
+        $controller->get();
+    } elseif ($requestUri[1] === 'remove') {
+        $controller->remove();
+    } elseif ($requestUri[1] === 'clear') {
+        $controller->clear();
+    }
 }
+?>
