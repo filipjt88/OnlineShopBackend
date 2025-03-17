9d9d42f4c175fbfc84f0e8993f02d0795b338003
filipjt88 <filip88ks@gmail.com>
2025-03-17 22:47:37 +0100

    save

---
+++ config/paypal.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'client_id' => 'TVOJ_PAYPAL_CLIENT_ID',
+    'secret' => 'TVOJ_PAYPAL_SECRET',
+    'sandbox' => true, // Postavi na false kad ideš u produkciju
+    'base_url' => 'https://api-m.sandbox.paypal.com', // Produkcija: https://api-m.paypal.com
+];
+?>
